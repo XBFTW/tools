c3db2e27c0b423dcde218273c8a3e2d817319293
XBFTW <74849594+XBFTW@users.noreply.github.com>
2025-05-14 02:07:54 -0700

    Create README.ml

---
+++ copysshkey/README.ml
@@ -0,0 +1,4 @@
+Made this cmd file because ssh-copy-id isn't on windows. 
+
+Just run this file, it assumes the user has a file named xbftwkey-public.pem in the ./ssh folder
+it prompts for username, ip, and alias to auto put it in the user's ssh config
